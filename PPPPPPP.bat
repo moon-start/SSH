498b05b1be55a749066fe11d0adc27c97f355519
moon-start <login0516mp4@gmail.com>
2019-12-19 08:49:50 +0800

    OK

---
+++ PPPPPPP.bat
@@ -0,0 +1,10 @@
+
+@rem   %L is replaced with the path of the directory
+@rem   
+@rem   The % sign needs to be escaped in a batch script with another
+@rem   % sign, thus %%L is used in the reg add command:
+@rem   環境變數
+
+@setx Path "%userprofile%\Desktop\SSH;%userprofile%\Desktop\PythonAPI\python.exe;%userprofile%\Desktop\PythonAPI\Scripts;"
+
+
