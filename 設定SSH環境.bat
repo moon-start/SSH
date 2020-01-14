ec2e934033f755494b8058fcdfa0209728b23f25
moon-start <login0516mp4@gmail.com>
2020-01-14 19:29:23 +0800

    All File

--- 設定SSH環境.bat
+++ 設定SSH環境.bat
@@ -1,10 +1,12 @@
-
+﻿@echo off
 @rem   %L is replaced with the path of the directory
 @rem   
 @rem   The % sign needs to be escaped in a batch script with another
 @rem   % sign, thus %%L is used in the reg add command:
 @rem   環境變數
 
+cls
 @setx Path "%userprofile%\Desktop\SSH;%userprofile%\Desktop\PythonAPI\python.exe;%userprofile%\Desktop\PythonAPI\Scripts;"
 
 
+pause
